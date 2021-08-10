468a83af8a521008527a8c432b15e73ad3d4d927
bananakiu <leandrenoelkiu@gmail.com>
2021-08-10 21:14:50 +0800

    add login page draft

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import LoginPage from './components/login/index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Testing
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        {/* turn into component */}
+        <nav>Navbar here</nav> 
       </header>
-    </div>
+      <main>
+        <LoginPage />
+      </main>
+    </>
   );
 }
 
---
+++ src/components/login/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return <>
+    <div className="
+    flex justify-center items-center
+    ">
+        {/* Login Menu */}
+        <form action="" className="
+        bg-white shadow-md rounded px-8 pt-6 pb-8
+        w-72 md:w-80
+        ">
+            <div className="mb-4">
+                <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
+                    Username
+                </label>
+                <input id="username" type="text" placeholder="Username" className="
+                shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline
+                "/>
+            </div>
+            <div className="mb-6">
+                <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
+                    Password
+                </label>
+                <input id="password" type="password" placeholder="*********" className="
+                shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline
+                "/>
+            </div>
+            <div class="flex items-center justify-between">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                    Sign In
+                </button>
+                <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
+                    Forgot Password?
+                </a>
+            </div>
+
+
+        </form>
+    </div>
+    </>
+};
+
+export default LoginPage;
+
