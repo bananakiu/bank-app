5661417e3463180cef636783cc9c06fb52fea5b1
bananakiu <leandrenoelkiu@gmail.com>
2021-08-12 16:57:48 +0800

    modify navbar options based on Wallet App

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -4,22 +4,31 @@ import bannerLogo from './../../assets/banner-logo-white.png';
 const NavBar = () => {
     return <>
         <nav class="
-        flex justify-between items-center w-full px-3 py-0 text-white
-        bg-blue-500
+        flex justify-between items-center
+        w-full px-3 py-0 
+        text-white bg-blue-500
         ">
-            <div id="logo">
-                <img src={bannerLogo} alt="NextBank logo" class="
-                h-10 my-0
-                "/>
+            <div id="navbar-left" class="flex justify-center items-center">
+                <div id="logo">
+                    <img src={bannerLogo} alt="NextBank logo" class="
+                    h-10 my-0 mr-4
+                    "/>
+                </div>
+                <div id="nav-items" class="
+                flex
+                text-center
+                ">
+                    <a class="flex-1 text-lg hover:bg-blue-600 py-4 px-6">Dashboard</a>
+                    <a class="flex-1 text-lg hover:bg-blue-600 py-4 px-6">Accounts</a>
+                    <a class="flex-1 text-lg hover:bg-blue-600 py-4 px-6">Records</a>
+                </div>
             </div>
-            <div id="nav-items" class="
-            flex
-            text-center
-            ">
-                <a class="flex-1 text-lg hover:bg-blue-600 py-4 px-6">Item 1</a>
-                <a class="flex-1 text-lg hover:bg-blue-600 py-4 px-6">Item 1</a>
-                <a class="flex-1 text-lg hover:bg-blue-600 py-4 px-6">Item 1</a>
+            <div id="navbar-right">
+                <a class="flex-1 text-lg hover:bg-blue-600 py-4 px-6">Logout</a>
+                <a class="flex-1 text-lg hover:bg-blue-600 py-4 px-6">Profile (settings,profile, acc image)</a>
             </div>
+            
+                
         </nav>
     </>
 };
